0850cdd63ab80add28e518a6234d5914c57eec46
LaamGinghong <351390560@qq.com>
2020-09-09 16:32:15 +0800

    202009091633

--- 手写代码/mvvm/mvvm.ts
+++ 手写代码/mvvm/mvvm.ts
@@ -1,44 +1,44 @@
 interface Handlers {
-    get(val: any): void;
+  get(val: any): void;
 
-    set(newVal: any, oldVal: any): void;
+  set(newVal: any, oldVal: any): void;
 }
 
 class Observer<T extends { [key: string]: any } = {}> {
-    private data: T;
+  private data: T;
 
-    constructor(data: T) {
-        this.data = data;
-        this.proxyData();
+  constructor(data: T) {
+    this.data = data;
+    this.proxyData();
+  }
+
+  observe(key: keyof T, handlers: Handlers): void {
+    const value = this.data[key];
+
+    Object.defineProperty(this.data, key, {
+      get: () => {
+        handlers.get(value);
+        return value;
+      },
+      set: (val) => {
+        if (val === value) return;
+        handlers.set(val, value);
+        this.data[key] = val;
+      },
+    });
+  }
+
+  private proxyData(): void {
+    const keys = Object.keys(this.data);
+    for (const key of keys) {
+      Object.defineProperty(this, key, {
+        get: () => {
+          return this.data[key];
+        },
+        set: (val) => {
+          this.data[key as keyof T] = val;
+        },
+      });
     }
-
-    observe(key: keyof T, handlers: Handlers): void {
-        const value = this.data[key];
-
-        Object.defineProperty(this.data, key, {
-            get: () => {
-                handlers.get(value);
-                return value;
-            },
-            set: (val) => {
-                if (val === value) return;
-                handlers.set(val, value);
-                this.data[key] = val;
-            },
-        });
-    }
-
-    private proxyData(): void {
-        const keys = Object.keys(this.data);
-        for (const key of keys) {
-            Object.defineProperty(this, key, {
-                get: () => {
-                    return this.data[key];
-                },
-                set: (val) => {
-                    this.data[key as keyof T] = val;
-                },
-            });
-        }
-    }
+  }
 }

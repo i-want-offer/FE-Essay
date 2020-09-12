f8873650ca7455cc7e92d7a98253e14e68c9da40
LaamGinghong <351390560@qq.com>
2020-09-12 22:42:56 +0800

    202009122242

--- 手写代码/Function/apply.js
+++ 手写代码/Function/apply.js
@@ -1,7 +1,7 @@
 Function.prototype.apply2 = function (that = window) {
   that.fn = this;
-  const args = arguments[1];
-  const result = args ? that.fn(...args) : that.fn();
+  const args = arguments[1] ?? [];
+  const result = that.fn(...args);
   delete that.fn;
   return result;
 };

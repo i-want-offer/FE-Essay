1cbcb13b3dd5ab38665854209d97ad4b69da448d
LaamGinghong <351390560@qq.com>
2020-09-14 16:24:20 +0800

    202009141624

--- 手写代码/event-emitter/event-emitter.ts
+++ 手写代码/event-emitter/event-emitter.ts
@@ -1,34 +1,34 @@
 interface Events {
-    [key: string]: Handler[];
+  [key: string]: Handler[];
 }
 
 type Handler = (...args: any) => void;
 
 class EventEmitter {
-    private events: Events = {};
+  private events: Events = {};
 
-    on(type: string, handler: Handler): void {
-        this.events[type] = this.events[type] ?? [];
-        this.events[type].push(handler);
-    }
+  on(type: string, handler: Handler): void {
+    this.events[type] = this.events[type] ?? [];
+    this.events[type].push(handler);
+  }
 
-    off(type: string): void {
-        if (!this.events[type]) return;
-        delete this.events[type];
-    }
+  off(type: string): void {
+    if (!this.events[type]) return;
+    delete this.events[type];
+  }
 
-    once(type: string, handler: Handler): void {
-        const fn = (...args: any): void => {
-            handler(args);
-            this.off(type);
-        };
+  once(type: string, handler: Handler): void {
+    const fn = (...args: any): void => {
+      handler(args);
+      this.off(type);
+    };
 
-        this.on(type, fn);
-    }
+    this.on(type, fn);
+  }
 
-    trigger(type: string, ...rest: any): void {
-        this.events[type]?.forEach((fn) => {
-            fn(...rest);
-        });
-    }
+  trigger(type: string, ...rest: any): void {
+    this.events[type]?.forEach((fn) => {
+      fn(...rest);
+    });
+  }
 }

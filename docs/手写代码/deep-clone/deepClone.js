9f0b28c722b0a643d393a2a736b97a3229669bd3
syou_yu <921393502@qq.com>
2020-12-08 22:19:34 +0800

    fix(deepClone): object data no returns

--- docs/手写代码/deep-clone/deepClone.js
+++ docs/手写代码/deep-clone/deepClone.js
@@ -1,13 +1,14 @@
 function deepClone(data) {
-  if (typeof data === "object") {
-    var result = Array.isArray(data) ? [] : {};
-    for (var key in result) {
-      if (typeof result[key] === "object") {
+  if (typeof data === 'object') {
+    const result = Array.isArray(data) ? [] : {};
+    for (let key in result) {
+      if (typeof result[key] === 'object') {
         result[key] = deepClone(data[key]);
       } else {
         result[key] = data[key];
       }
     }
+    return result;
   } else {
     return data;
   }
